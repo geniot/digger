835e00ad86907ee1c987122013e8fd06a9f4f523
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2022-12-13 15:00:13 +0100

    Eat field left-right

--- internal/impl/gui/rnd/field.go
+++ internal/impl/gui/rnd/field.go
@@ -10,29 +10,36 @@ import (
 )
 
 type Field struct {
-	background *sdl.Surface
-	hBlob      *sdl.Surface
-	scene      *Scene
+	background   *sdl.Surface
+	tunnelBlob   *sdl.Surface
+	endLeftBlob  *sdl.Surface
+	endRightBlob *sdl.Surface
+	scene        *Scene
 }
 
 func NewField(scn *Scene) *Field {
-	hB, _ := img.LoadRW(resources.GetResource("hfblob.png"), true)
+	tunnelB, _ := img.LoadRW(resources.GetResource("blob1.png"), true)
+	endLeftB, _ := img.LoadRW(resources.GetResource("blob2.png"), true)
+	endRightB, _ := img.LoadRW(resources.GetResource("blob3.png"), true)
 	bgrTile, _ := img.LoadRW(resources.GetResource("cback1.png"), true)
+
 	defer bgrTile.Free()
+
 	bgrSurface, _ := sdl.CreateRGBSurfaceWithFormat(0,
 		glb.SCREEN_LOGICAL_WIDTH,
 		glb.SCREEN_LOGICAL_HEIGHT,
 		32, ctx.SurfaceIns.Format.Format)
-	for i := 0; i < glb.SCREEN_LOGICAL_WIDTH/20; i++ {
-		for j := 0; j < glb.SCREEN_LOGICAL_HEIGHT/4; j++ {
+
+	for i := 0; i < glb.SCREEN_LOGICAL_WIDTH/int(bgrTile.W); i++ {
+		for j := 0; j < glb.SCREEN_LOGICAL_HEIGHT/int(bgrTile.H); j++ {
 			bgrTile.Blit(
-				&sdl.Rect{0, 0, 20, 4},
+				&sdl.Rect{0, 0, bgrTile.W, bgrTile.H},
 				bgrSurface,
-				&sdl.Rect{int32(i * 20), int32(j * 4), 20, 4})
+				&sdl.Rect{int32(i * int(bgrTile.W)), int32(j * int(bgrTile.H)), bgrTile.W, bgrTile.H})
 		}
 	}
 
-	return &Field{bgrSurface, hB, scn}
+	return &Field{bgrSurface, tunnelB, endLeftB, endRightB, scn}
 }
 
 func (field Field) Render() {
@@ -42,8 +49,17 @@ func (field Field) Render() {
 }
 
 func (field *Field) Step(n uint64) {
-	blobRect := sdl.Rect{int32(math.Mod(float64(field.scene.digger.offsetX), 4)), 0, 1, 20}
-	diggerRect := sdl.Rect{field.scene.digger.offsetX + 20, field.scene.digger.offsetY - 20, 20, 20}
-	field.hBlob.Blit(&blobRect, field.background, &diggerRect)
+	sourceTunnelRect := sdl.Rect{int32(math.Mod(float64(field.scene.digger.offsetX), float64(field.tunnelBlob.W))), 0, 1, field.tunnelBlob.H}
+	if field.scene.digger.direction == glb.RIGHT {
+		targetTunnelRect := sdl.Rect{field.scene.digger.offsetX + glb.CELL_WIDTH - field.tunnelBlob.W, field.scene.digger.offsetY - glb.CELL_HEIGHT, glb.CELL_WIDTH, glb.CELL_HEIGHT}
+		targetEndRect := sdl.Rect{field.scene.digger.offsetX + glb.CELL_WIDTH - field.endRightBlob.W, field.scene.digger.offsetY - glb.CELL_HEIGHT, glb.CELL_WIDTH, glb.CELL_HEIGHT}
+		field.tunnelBlob.Blit(&sourceTunnelRect, field.background, &targetTunnelRect)
+		field.endRightBlob.Blit(nil, field.background, &targetEndRect)
+	} else if field.scene.digger.direction == glb.LEFT {
+		targetTunnelRect := sdl.Rect{field.scene.digger.offsetX + field.tunnelBlob.W, field.scene.digger.offsetY - glb.CELL_HEIGHT, glb.CELL_WIDTH, glb.CELL_HEIGHT}
+		targetEndRect := sdl.Rect{field.scene.digger.offsetX, field.scene.digger.offsetY - glb.CELL_HEIGHT, glb.CELL_WIDTH, glb.CELL_HEIGHT}
+		field.tunnelBlob.Blit(&sourceTunnelRect, field.background, &targetTunnelRect)
+		field.endLeftBlob.Blit(nil, field.background, &targetEndRect)
+	}
 
 }
